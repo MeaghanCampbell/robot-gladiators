9c1d61fcfb44785fdb25bdc7bbdafb159a04ebe7
Meaghan Campbell <emeaghancampbell@gmail.com>
2020-12-19 19:59:59 -0600

    initial fight functionality

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,17 +1,53 @@
-// this creates a function named "fight"
-
 var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
 
-console.log(playerName)
+var enemyName = "Roberto";
+var enemyHealth = 50;
+var enemyAttack = 12;
 
-console.log("This logs a string, good for leaving yourself a message");
+var fight = function() {
+    // Alert players that they are starting the round
+    window.alert("Welcome to Robot Gladiators!");
+  
+    //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
+    
+    enemyHealth = enemyHealth - playerAttack;
+  
+    // Log a resulting message to the console so we know that it worked.
 
-console.log(10+10);
+    console.log(
+        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+      );
 
-console.log("Our robots name is" + playerName);
+    // check enemy's health
 
-function fight() {
-    window.alert("The fight has begun!");
-}
+    if (enemyHealth <= 0) {
+    window.alert(enemyName + " has died!");
+    } 
+    else {
+    window.alert(enemyName + " still has " + enemyHealth + " health left.");
+    }
+  
+  
+    // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
 
-// fight();
+    playerHealth = playerHealth - enemyAttack;
+  
+    // Log a resulting message to the console so we know that it worked.
+
+    console.log(
+        enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+    );
+
+    // check player's health
+    
+    if (playerHealth <= 0) {
+    window.alert(playerName + " has died!");
+    } 
+    else {
+    window.alert(playerName + " still has " + playerHealth + " health left.");
+    }
+};
+
+fight();
