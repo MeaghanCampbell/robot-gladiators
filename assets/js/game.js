a934bd8653c2548356541d37ab31fe91087cf33e
Meaghan Campbell <emeaghancampbell@gmail.com>
2020-12-20 12:26:26 -0600

    multiple robots added to the loop

--- assets/js/game.js
+++ assets/js/game.js
@@ -16,9 +16,9 @@ var enemyAttack = 12;
 
 // fight function
 var fight = function(enemyName) {
-  // Alert players that they are starting the round
-  window.alert("Welcome to Robot Gladiators!");
 
+  while(enemyHealth > 0) {
+  
   // ask player if they'd like to fight or run
   var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
 
@@ -68,11 +68,14 @@ var fight = function(enemyName) {
   } else {
     window.alert("You need to pick a valid option. Try again!");
   }
+}
 };
 
 // for loop 
 
 for(var i = 0; i < enemyNames.length; i++) {
-  fight(enemyNames[i]);
+  var pickedEnemyName = enemyNames[i];
+  enemyHealth = 50;
+  fight(pickedEnemyName);
 }
 
